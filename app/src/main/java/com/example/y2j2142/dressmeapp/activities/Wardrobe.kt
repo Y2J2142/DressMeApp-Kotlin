3e4a5f7ce77585b4c4cafc67cc9a7ab3e941e3fe
Unknown <Y2J2142@hotmail.com>
2018-02-24 00:29:47 +0100

    Wardrobe activity added

---
+++ app/src/main/java/com/example/y2j2142/dressmeapp/activities/Wardrobe.kt
@@ -0,0 +1,13 @@
+package com.example.y2j2142.dressmeapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.y2j2142.dressmeapp.R
+
+class Wardrobe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wardrobe)
+    }
+}
