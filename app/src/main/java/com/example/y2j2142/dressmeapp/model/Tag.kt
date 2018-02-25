fd00423b7c9d66fcc56d769bfc2f862a105b367e
Unknown <Y2J2142@hotmail.com>
2018-02-25 18:39:47 +0100

    Tag class update
    companion object added and value types changed

--- app/src/main/java/com/example/y2j2142/dressmeapp/model/Tag.kt
+++ app/src/main/java/com/example/y2j2142/dressmeapp/model/Tag.kt
@@ -1,3 +1,14 @@
 package com.example.y2j2142.dressmeapp.model
 
-data class Tag(val category:String = "", val name:String="", val weight: Int=0)
+data class Tag(val category:String = "", val name:String="", val weight: Long = 0, val id: Long = 0, val imagePath: String = "")
+{
+    companion object {
+        const val COLUMN_ID = "id"
+        const val TABLE_NAME = "tags"
+        const val CATEGORY = "category"
+        const val NAME = "name"
+        const val WEIGHT = "weight"
+        const val IMAGE_PATH = "image_path"
+    }
+
+}
